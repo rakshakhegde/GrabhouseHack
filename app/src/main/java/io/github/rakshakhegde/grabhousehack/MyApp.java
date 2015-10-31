e3d78857ea44bcda44da2d9caac105ab857cf410
Rakshak R.Hegde <rakshakhegde@gmail.com>
2015-10-31 18:21:04 +0530

    Parse & MD updates
    - Parse Push Integrated
    - TIMELINE.MD has some details now

--- app/src/main/java/io/github/rakshakhegde/grabhousehack/MyApp.java
+++ app/src/main/java/io/github/rakshakhegde/grabhousehack/MyApp.java
@@ -3,6 +3,7 @@ package io.github.rakshakhegde.grabhousehack;
 import android.app.Application;
 
 import com.parse.Parse;
+import com.parse.ParseInstallation;
 
 /**
  * Created by Rakshak.R.Hegde on 10/31/2015.
@@ -15,5 +16,6 @@ public class MyApp extends Application {
 		Parse.enableLocalDatastore(this);
 
 		Parse.initialize(this, "tNWxfthiYfeg6R1ity9k1Ps5TCsW2J9GATNKtbxi", "CYIFz0vSkC1qdCnMwimlGdocBaTPU0d6iu4JLj5r");
+		ParseInstallation.getCurrentInstallation().saveInBackground();
 	}
 }
